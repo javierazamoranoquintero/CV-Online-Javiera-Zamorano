29e684dc25f076a57ffb9762e88ab52e672077c0
javiera zamorano <javierazamorano@MacBook-Pro-de-javiera.local>
2026-08-16 17:23:32 -0400

    Fix: base path para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/CV-Online-Javiera-Zamorano/',
     server: {
         port: 5173,
         host: true,
         open: false,
+        watch: {
+            usePolling: true,
+        },
     },
     build: {
         outDir: 'dist',
